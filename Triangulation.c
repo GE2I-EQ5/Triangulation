58beda1c57feccab06555b4d4fdf86cf5161f9b3
GE2I-EQ5 <71700072+GE2I-EQ5@users.noreply.github.com>
2020-10-21 15:32:44 +0200

    Update Triangulation.c

--- Triangulation.c
+++ Triangulation.c
@@ -3,12 +3,13 @@
 
 int main()
 {
-  float tc=1, tb=1, cb=1;
-  float xc = sqrt(pow(tc,2));
-  float yc = (pow(tb,2) + pow(tc,2) - pow(cb,2)) / (2*tb);
+  float tc=1, tb=1, cb=1; //Distances
+  const int v_son = 340; //Vitesse du son dans l'air
+  float yc = (pow(tb,2) + pow(tc,2) - pow(cb,2)) / (2*tb); //Yc
+  float xc = sqrt(pow(tc,2)-pow(yc,2)); //Xc
+  
   printf("XC : %f\n", xc);
   printf("YC : %f\n", yc);
 
   return 0;
 }
-
