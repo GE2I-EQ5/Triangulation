3f5ac05066be65090284cae28a3761fdb3881784
GE2I-EQ5 <71700072+GE2I-EQ5@users.noreply.github.com>
2020-10-21 14:41:01 +0200

    v1

--- Triangulation.c
+++ Triangulation.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-  /* code */
+  float tc=1, tb=1, cb=1;
+  float xc = sqrt(pow(tc,2));
+  float yc = (pow(tb,2) + pow(tc,2) - pow(cb,2)) / (2*tb);
+  printf("XC : %f\n", xc);
+  printf("YC : %f\n", yc);
+
   return 0;
 }
+
